db2a03b8a7bf4370422b0489b9381a10f67b28a9
odxdx <155006942+odxdx@users.noreply.github.com>
2026-01-21 16:06:45 +0200

    Delete backend directory

--- backend/models/doctorModel.js
+++
@@ -1,8 +0,0 @@
-import db from '../db.js';
-
-export const getDoctorsList = (req, res) => {
-    db.query(`SELECT * FROM doctors`, (err, results) => {
-        if (err) return res.status(500).json({ error: err.message });
-        res.json(results);
-    });
-};
